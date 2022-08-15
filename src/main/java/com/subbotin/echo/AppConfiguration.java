7691a38f562e54121f93eb91ae852178921b7e7a
subbotin-n-o <subbotin.n.o@gmail.com>
2022-08-15 23:04:43 +0400

    add SpringApp.java, AppConfiguration.java

--- src/main/java/com/subbotin/echo/AppConfiguration.java
+++ src/main/java/com/subbotin/echo/AppConfiguration.java
@@ -1,19 +1,22 @@
 package com.subbotin.echo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class Main {
-    public static void main(String[] args) throws TelegramApiException {
-        /*
-        Регистрация бота:
-        - создаем обьект класса TelegramBotsApi, который позволяет прописывать различные параметры для ботов;
-        - создаем экземпляр нашего бота;
-        - регистрируем бот.
-        */
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public PomodoroBot pomodoroBot() {
+        return new PomodoroBot();
+    }
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(PomodoroBot pomodoroBot) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        PomodoroBot pomodoroBot = new PomodoroBot();
         telegramBotsApi.registerBot(pomodoroBot);
         new Thread(() -> {
             try {
@@ -22,5 +25,6 @@ public class Main {
                 e.printStackTrace();
             }
         }).run();
+        return telegramBotsApi;
     }
 }
---
+++ src/main/java/com/subbotin/echo/SpringApp.java
@@ -0,0 +1,11 @@
+package com.subbotin.echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class);
+    }
+}
